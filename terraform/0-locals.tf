ece682e984df5cd786fb53a5b8cf806fc5248de4
alskorov <skorovalex@gmail.com>
2024-11-14 19:56:24 +0200

    added vpc name

--- terraform/0-locals.tf
+++ terraform/0-locals.tf
@@ -5,4 +5,5 @@ locals {
     zone2       = "us-east-2b"
     eks_name    = "demo"
     eks_version = "1.31"
+    vpc_name    = "demo"
 }
--- terraform/2-vpc.tf
+++ terraform/2-vpc.tf
@@ -3,5 +3,10 @@ resource "aws_vpc" "main" {
 
   enable_dns_support = true
   enable_dns_hostnames = true
-  
+
+  tags = {
+    Name = "${local.vpc_name}-vpc"
+  }
+
+
 }
