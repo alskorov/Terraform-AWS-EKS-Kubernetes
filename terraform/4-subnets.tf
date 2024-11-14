4549028213869b6847a99ce79d969b269d7844cc
alskorov <skorovalex@gmail.com>
2024-11-14 17:47:45 +0200

    fixed typo in subnet ip

--- terraform/4-subnets.tf
+++ terraform/4-subnets.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "private_zone2" {
 
 resource "aws_subnet" "public_zone1" {
     vpc_id              = aws_vpc.main.id
-    cidr_block          = "10.0.64.0/19"
+    cidr_block          = "10.1.64.0/19"
     availability_zone   = local.zone1
     map_public_ip_on_launch = true
 
@@ -37,7 +37,7 @@ resource "aws_subnet" "public_zone1" {
 
 resource "aws_subnet" "public_zone2" {
     vpc_id                      = aws_vpc.main.id
-    cidr_block                  = "10.0.96.0/19"
+    cidr_block                  = "10.1.96.0/19"
     availability_zone           = local.zone2
     map_public_ip_on_launch     = true
 
