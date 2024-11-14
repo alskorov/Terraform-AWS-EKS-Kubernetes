e91ec0ce7fa45098481286822c5d8d498dc65b1d
alskorov <skorovalex@gmail.com>
2024-11-14 17:42:46 +0200

    fixed typo

--- terraform/4-subnets.tf
+++ terraform/4-subnets.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "private_zone1" {
     tags = {
       "Name"                                                        = "${local.env}-private-${local.zone1}"
       "kubernetes.io/role/internal-elb"                             = "1"
-      "kubernetes.io/cluster/${lcoal.env}-${local.eks_name}"        = "owend"
+      "kubernetes.io/cluster/${local.env}-${local.eks_name}"        = "owend"
     }   
 }
 
@@ -18,15 +18,15 @@ resource "aws_subnet" "private_zone2" {
     tags = {
       "Name"                                                        = "${local.env}-private-${local.zone2}"
       "kubernetes.io/role/internal-elb"                             = "1"
-      "kubernetes.io/cluster/${lcoal.env}-${local.eks_name}"        = "owend"
+      "kubernetes.io/cluster/${local.env}-${local.eks_name}"        = "owend"
     }   
 }
 
 resource "aws_subnet" "public_zone1" {
-    vpc_id              = vpc_id.main.id
+    vpc_id              = aws_vpc.main.id
     cidr_block          = "10.0.64.0/19"
     availability_zone   = local.zone1
-    map_customer_owned_ip_on_launch = true
+    map_public_ip_on_launch = true
 
     tags = {
       "Name" = "${local.env}-public-${local.zone1}"
@@ -36,10 +36,10 @@ resource "aws_subnet" "public_zone1" {
 }
 
 resource "aws_subnet" "public_zone2" {
-    vpc_id              = vpc_id.main.id
-    cidr_block          = "10.0.96.0/19"
-    availability_zone   = local.zone2
-    map_customer_owned_ip_on_launch = true
+    vpc_id                      = aws_vpc.main.id
+    cidr_block                  = "10.0.96.0/19"
+    availability_zone           = local.zone2
+    map_public_ip_on_launch     = true
 
     tags = {
       "Name" = "${local.env}-public-${local.zone2}"
