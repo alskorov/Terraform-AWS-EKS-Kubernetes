76361a3e6e35c94dac24f8e369297e99e5733e52
alskorov <skorovalex@gmail.com>
2024-11-14 17:10:06 +0200

    fix typo in providers.tf

--- terraform/1-providers.tf
+++ terraform/1-providers.tf
@@ -1,14 +1,14 @@
-provider "aws {
-    region  = local.region
+provider "aws" {
+  region    =   local.region
 }
 
 terraform {
-    required_version    = ">= 1.0"
+    required_version = ">= 1.0"
 
     required_providers {
-        aws = {
-            source      = "hashicorp/aws"
-            version     = "~> 5.75" 
-        }
+      aws = {
+        source  =   "hashicorp/aws"
+        version =   "~> 5.75"
+      }
     }
 }
