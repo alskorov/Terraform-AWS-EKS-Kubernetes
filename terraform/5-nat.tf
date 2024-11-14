14ca7b9c937cd64ffe6c3d66ac79f6c0dc28b477
alskorov <skorovalex@gmail.com>
2024-11-14 17:46:33 +0200

    forgot nat id in resource

--- terraform/5-nat.tf
+++ terraform/5-nat.tf
@@ -9,7 +9,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_zone1
+  subnet_id     = aws_subnet.public_zone1.id
 
   tags = {
     Name = "${local.env}-nat"
