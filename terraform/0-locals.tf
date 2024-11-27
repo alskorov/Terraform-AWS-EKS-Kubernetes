0f0af4d5244680035ac76f711008741d3ff7e630
alskorov <skorovalex@gmail.com>
2024-11-27 23:04:18 +0200

    added jenkins server

--- terraform/0-locals.tf
+++ terraform/0-locals.tf
@@ -5,4 +5,11 @@ locals {
     zone2       = "us-east-2b"
     eks_name    = "demo"
     eks_version = "1.31"
+    
+    //EC2 Jenkins 
+    key-name        = "Aleksey"
+    instance-name   = "Jenkins-server"
+    sg-name         = "Jenkins-sg"
+    iam-role        = "Jenkins-iam-role"
+    
 }
